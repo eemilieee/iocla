f65ec1db05c5f5601dd3e00cf3024b54e4969c7a
Emilia Arpasanu <emyarpa@gmail.com>
2022-03-04 18:42:10 +0200

    Rezolvarea exercitiului 4 - pentru permutarea circulara a bitilor am memorat valoarea bitului eliminat, am shiftat bitii cu 1 pozitie si am setat valoarea bitului care reprezinta "golul" cu valoarea celui eliminat folosind o masca corespunzatoare; - procesul se repeta de atatea ori cat este necesar;
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -5,6 +5,26 @@ void rotate_left(int *number, int bits)
 	/* TODO */
 	(void) number;
 	(void) bits;
+	int bit,masca,i;
+	for(i=1; i<=bits; i++)
+	{
+		if((*number)&(1<<31))
+			bit=1;
+		else
+			bit=0;
+		*number=(*number)<<1;
+		if(bit)
+			{
+				masca=1;
+				*number=(*number)|masca;
+			}
+		else
+		{
+			masca=1;
+			masca=~masca;
+			*number=(*number)&masca;
+		}
+	}
 }
 
 void rotate_right(int *number, int bits)
@@ -12,12 +32,37 @@ void rotate_right(int *number, int bits)
 	/* TODO */
 	(void) number;
 	(void) bits;
+	int bit,masca,i;
+	for(i=1; i<=bits; i++)
+	{
+		if((*number)&1)
+			bit=1;
+		else
+			bit=0;
+		*number=(*number)>>1;
+		if(bit)
+			{
+				masca=1<<31;
+				*number=(*number)|masca;
+			}
+		else
+		{
+			masca=1<<31;
+			masca=~masca;
+			*number=(*number)&masca;
+		}
+	}
 }
 
 int main()
 {
 	/* TODO: Test functions */
-
+	int n,b,m;
+	scanf("%d%d",&n,&b);
+	m=n;
+	rotate_left(&n,b);
+	rotate_right(&m,b);
+	printf("%d %d\n",n,m);
 	return 0;
 }
 
