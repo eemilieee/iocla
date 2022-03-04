f5aeb3bcdf6e0039a9c2c946eec22d55473b11a0
Emilia Arpasanu <emyarpa@gmail.com>
2022-03-04 21:54:14 +0200

    Rezolvarea exercitiului 1 - pentru parcurgerea adreselor din octet in octet/din 2 in 2 octeti/din 4 in 4 octeti am folosit un pointer de tip unsigned char/short int/int ce pointeaza catre inceputul vectorului, iterand de sizeof(vector)/sizeof(pointer) ori si afisand la fiecare pas pointer+pas si *(pointer+pas)
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex1/ex1.c
+++ laborator/content/operatii-memorie-gdb/ex1/ex1.c
@@ -9,6 +9,19 @@
 
 int main() {
     int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
-
+    int n=sizeof(v)/sizeof(int), i, *r;
+    unsigned char *p=(unsigned char *)v;
+    short *q=(short *)v;
+    //adrese din octet in octet
+    for(i=0; i<sizeof(v); i++)
+        printf("%p -> OX%x\n",p+i,*(p+i));
+    printf("\n");
+    //adrese din 2 in 2 octeti
+    for(i=0; i<sizeof(v)/2; i++)
+         printf("%p -> OX%x\n",q+i,*(q+i));
+    printf("\n");
+    //adrese din 4 in 4 octeti
+    for(r=v; r<v+n; r++)
+        printf("%p -> OX%x\n",r,*r);
     return 0;
 }
