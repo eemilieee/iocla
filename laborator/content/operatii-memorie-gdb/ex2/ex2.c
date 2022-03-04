0c561fb32633180ad21574f966e245d4bddf69ff
Emilia Arpasanu <emyarpa@gmail.com>
2022-03-04 22:12:44 +0200

    Rezolvarea exercitiului 2 - pentru stergerea primei aparitii a subsirului in sir am copiat caracterele imediat urmatoare subsirului la adresa aparitiei acestuia
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex2/ex2.c
+++ laborator/content/operatii-memorie-gdb/ex2/ex2.c
@@ -2,14 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern)
+{
+	strcpy(strstr(s,pattern),strstr(s,pattern)+strlen(pattern));
+	return s;
+}
 
 int main(){
-	char *s = "Ana are mere";
+	char s[] = "Ana are mere";
 	char *pattern = "re";
 
 	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
