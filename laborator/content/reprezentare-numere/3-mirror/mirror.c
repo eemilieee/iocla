a2d9bf903a1c36e21c39a547965fcbf43dd69801
Emilia Arpasanu <emyarpa@gmail.com>
2022-03-04 18:34:28 +0200

    Rezolvarea exercitiului 3 - am parcurs sirul de caractere pana la jumatate si am interschimbat elementele (accesate cu operatiile cu pointeri) astfel: primul cu ultimul, al 2-lea cu penultimul etc.;
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -2,16 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+int my_strlen(const char *str)
+{
+	/* TODO */
+	(void) str;
+	const char *p;
+	int nr=0;
+	p=str;
+	while(*p)
+	{
+		nr++;
+		p++;
+	}
+	return nr;
+}
+
 void mirror(char *s)
 {
 	/* TODO */
 	(void) s;
+	int i;
+	char aux;
+	for(i=0; i<my_strlen(s)/2; i++)
+	{
+		aux=*(s+i);
+		*(s+i)=*(s+my_strlen(s)-1-i);
+		*(s+my_strlen(s)-1-i)=aux;
+	}
 }
 
 int main()
 {
 	/* TODO: Test function */
-
+	char s[100];
+	fgets(s,100,stdin);
+	strcpy(s+my_strlen(s)-1,s+my_strlen(s));
+	mirror(s);
+	printf("%s\n",s);
 	return 0;
 }
 
