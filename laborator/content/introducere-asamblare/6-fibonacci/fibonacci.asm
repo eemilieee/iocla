fcbf0197f3812efc181c550ba6566f9895bb8e2b
Emilia Arpasanu <emyarpa@gmail.com>
2022-03-28 03:58:08 +0300

    Exercitiul 6
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -9,4 +9,19 @@ main:
 
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
 
+    mov ebx, 0
+    mov ecx, 1
+
+loop:
+    cmp eax, 2
+    je out
+    mov edx, ebx
+    add edx, ecx
+    mov ebx, ecx
+    mov ecx, edx
+    sub eax, 1
+    jmp loop
+
+out:
+    PRINTF32 `%d\n\x0`, edx
     ret
