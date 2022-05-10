4bec2ea80fa21446e2359aed04545a4c72e95999
Emilia Arpasanu <emyarpa@gmail.com>
2022-05-10 13:45:17 +0300

    Exercitiul 2: main.c
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/interactiune-c-assembly/1-2-max-c-calls/main.c
+++ laborator/content/interactiune-c-assembly/1-2-max-c-calls/main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-unsigned int get_max(unsigned int *arr, unsigned int len);
+unsigned int get_max(unsigned int *arr, unsigned int len, unsigned int *poz);
 
 int main(void)
 {
 	unsigned int arr[] = { 19, 7, 129, 87, 54, 218, 67, 12, 19, 99 };
 	unsigned int max;
 
-	max = get_max(arr, 10);
+	unsigned int poz = 0;
+
+	max = get_max(arr, 10, &poz);
 
 	printf("max: %u\n", max);
+
+	printf("poz: %u\n", poz);
 
 	return 0;
 }
