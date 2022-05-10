1ce1166aceff62c9e9165f0c698433f3ead364d4
Emilia Arpasanu <emyarpa@gmail.com>
2022-05-10 13:18:30 +0300

    Exercitiul 2
    Signed-off-by: Emilia Arpasanu <emyarpa@gmail.com>

--- laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
+++ laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
@@ -11,15 +11,19 @@ get_max:
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
+	; [ebp + 16] is the position of the max value
 
 	mov ebx, [ebp+8]
 	mov ecx, [ebp+12]
+	mov edx, [ebp + 16]
 	xor eax, eax
 
 compare:
 	cmp eax, [ebx+ecx*4-4]
 	jge check_end
 	mov eax, [ebx+ecx*4-4]
+	mov dword[edx], ecx
+	sub dword[edx], 1
 check_end:
 	loopnz compare
 
